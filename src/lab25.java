fe4170fd6696b61bc2cd6dd237b6c4c7a9b831d0
TrinhDang <trinhdang1011@gmail.com>
2021-01-07 23:16:29 +0700

    lab2.5 final

--- src/lab25.java
+++ src/lab25.java
@@ -8,22 +8,20 @@ public class lab25 {
         double gia1= 15000; // <= 1 km
         double gia2= 13500; // <= 5 km
         double gia3= 11000; //  > 5 km
-        double tongtien = 0;
         if (km<=1){
-            tongtien = km * gia1;
-            System.out.println("Tong tien la: "+tongtien+"vnd");
-        }else
-        if(km<=5){
-            tongtien = gia1 + gia2 * (km-1) ;
-            System.out.println("Tong tien la: "+tongtien+"vnd");
+            double tongtien1 = km * gia1;
+            System.out.println("Tong tien la: "+tongtien1+"vnd");
+        }else if(km<=5){
+            double tongtien2 = gia1 + gia2 * (km-1) ;
+            System.out.println("Tong tien la: "+tongtien2+"vnd");
         }
-        if(km>5){
-            tongtien = gia1 + gia2*4 + gia3*(km-5);
-            System.out.println("Tong tien la: "+tongtien+"vnd");
-        }
-        if(km>120){
-            tongtien= tongtien - tongtien * 10/100;
-            System.out.println("Tong tien la: "+tongtien+"vnd");
+        if(km>5 && km<=120) {
+            double tongtien3 = gia1 + gia2 * 4 + gia3 * (km - 5);
+            System.out.println("Tong tien la: " + tongtien3 + "vnd");
+        }else if(km>120){
+            double i = 15000 + gia2 * 4 + gia3 * (km - 5);
+            double tongtien4= i - i * 10/100;
+            System.out.println("Tong tien la: "+tongtien4+"vnd");
         }
 
 
